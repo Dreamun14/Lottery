fcd45a816541ceba658c36d65b926dfba31ed3c4
Krystalees <237504+Krystalees@users.noreply.github.com>
2021-06-19 10:50:33 -0700

    Update Lottery.py
    Design plan so far...

--- Lottery.py
+++ Lottery.py
@@ -1,4 +1,13 @@
+
+# Testing Section
 the_world_is_flat = True
 tempature = 85
 text = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
-print(text)
+
+# Design Plan
+# Print information about this game to the screen
+# Generate a random number from 1-20(hidden)
+# Input a number from 1-20
+# 3 chances (3 lives)
+# Does the winning number your number, if it does, then you win, else you lose
+# show the winning number if you lose
