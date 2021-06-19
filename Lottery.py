80d00d188c743f656cc7b0bf4de90e08f74badfe
Dreamun14 <86132974+Dreamun14@users.noreply.github.com>
2021-06-19 15:22:12 -0500

    Update Lottery.py
    Created life loop for your fruit loops

--- Lottery.py
+++ Lottery.py
@@ -3,12 +3,22 @@ import random
 # Design Plan
 # Print information about this game to the screen
 print('Come one, come all! Welcome to my lottery game!\nHere you can test your luck and see if you can make a fortune!\nPlease remember to choose a number. In this lottery game, you can choose a number between 1 through 20.\n \nChoose wisely and good luck!')
-# Input a number from 1-20
-pickednumber = input("Please enter a number :> ")
-# Generate a random number from 1-20(hidden)
-winningnumber = random.randrange(1, 20)
-# 3 chances (3 lives)
-# Does the winning number your number, if it does, then you win, else you lose
-# show the winning number
-print(f"Thank your for playing, the winning number was {winningnumber}")
-input("Press any key to exit")
+# Lives
+lives = 3
+# Loop start
+while lives > 0:
+    print(f"You currently have {lives} lives left")
+    lives = lives - 1
+    # Input a number from 1-20
+    pickednumber = input("Please enter a number :> ")
+    # Generate a random number from 1-20(hidden)
+    winningnumber = random.randrange(1, 20)
+    # 3 chances (3 lives)
+    # Does the winning number equal your number, if it does, then you win, else you lose
+    if pickednumber == winningnumber:
+        print('Congratulations! Luck was on your side!')
+    else:
+        print('I\'m sorry, luck was not on your side today...')
+    # show the winning number
+    print(f"Thank your for playing, the winning number was {winningnumber}")
+    input("Press any key to continue")
