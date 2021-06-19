515138e87835ee47261750f7d68d8fe6fa609da8
Dreamun14 <86132974+Dreamun14@users.noreply.github.com>
2021-06-19 14:13:02 -0500

    Update Lottery.py
    Created a random winning number

--- Lottery.py
+++ Lottery.py
@@ -1,9 +1,12 @@
+# Import section
+import random 
 # Design Plan
 # Print information about this game to the screen
 print('Come one, come all! Welcome to my lottery game!\nHere you can test your luck and see if you can make a fortune!\nPlease remember to choose a number. In this lottery game, you can choose a number between 1 through 20.\n \nChoose wisely and good luck!')
 # Input a number from 1-20
 pickednumber = input("Please enter a number :> ")
 # Generate a random number from 1-20(hidden)
+winningnumber = random.randrange(1, 20)
 # 3 chances (3 lives)
 # Does the winning number your number, if it does, then you win, else you lose
 # show the winning number if you lose
