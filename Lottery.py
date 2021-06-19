71be7b95e095e602340ea4c592618d03da94291e
Dreamun14 <86132974+Dreamun14@users.noreply.github.com>
2021-06-19 13:39:13 -0500

    Update Lottery.py
    I wrote some quotations.

--- Lottery.py
+++ Lottery.py
@@ -1,11 +1,6 @@
-
-# Testing Section
-the_world_is_flat = True
-tempature = 85
-text = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
-
 # Design Plan
 # Print information about this game to the screen
+print('Come one, come all! Welcome to my lottery game!\nHere you can test your luck and see if you can make a fortune!\nPlease remember to choose a number. In this lottery game, you can choose a number between 1 through 20.\n \nChoose wisely and good luck!')
 # Generate a random number from 1-20(hidden)
 # Input a number from 1-20
 # 3 chances (3 lives)
